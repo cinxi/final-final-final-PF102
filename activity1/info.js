e53e188853fd2762cf5f27b5d2ce8e691555dbac
unknown <fernandezclaire64@gmail.com>
2024-03-21 23:13:52 +0800

    My first commit

--- activity1/info.js
+++ activity1/info.js
@@ -5,7 +5,7 @@ const leftDiv = (title, title2, para1, para2, btn1, btn2, btn3, btn4) => {
                     <p>${para1}</p>
                     <br>
                      <h1>${title2}</h1>
-                    <p>${para2}</p>
+         git add           <p>${para2}</p>
                     <br>
                      <button>${btn1}</button>
                      <button>${btn2}</button>                   
